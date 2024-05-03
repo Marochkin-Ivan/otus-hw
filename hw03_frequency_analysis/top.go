4c6bab7a0318968b57dba7e7d772c310186522cf
Иван Марочкин <marochkin_i@rusklimat.ru>
2024-05-03 13:49:06 +0300

    complete hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -2,27 +2,68 @@ package hw03frequencyanalysis
 
 import (
 	"github.com/wasilibs/go-re2"
+	"sort"
 	"strings"
 )
 
-const puncts = `,.!?;:-"'¡¿`
+const (
+	punctuation = `,.!?;:-"'¡¿`
+	dash        = "-"
+)
 
-var wordSurroundedPunctuation = re2.MustCompile(`^[[:punct:]]+[[:graph:]]+[[:punct:]]+$`)
+var wordSurroundedPunctuation = re2.MustCompile(`^[[:punct:]]*[^[:punct:]]+[[:punct:]]*$`)
+
+type rating map[string]int
+
+func (r rating) keys() []string {
+	keys := make([]string, 0, len(r))
+
+	for k := range r {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
+
+func (r rating) sortByKeys() []string {
+	keys := r.keys()
+	sort.Strings(keys)
+	return keys
+}
+
+func (r rating) sortByKeysAndValue() []string {
+	keys := r.sortByKeys()
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return r[keys[i]] > r[keys[j]]
+	})
+
+	return keys
+}
 
 func Top10(input string) []string {
 	words := strings.Fields(input)
 
-	rating := make(map[string]int)
+	rate := make(rating)
 	for _, word := range words {
 		if wordSurroundedPunctuation.MatchString(word) {
 			word = correctWord(word)
 		}
-		rating[word]++
+		if word == dash {
+			continue
+		}
+		rate[word]++
 	}
 
-	return nil
+	res := rate.sortByKeysAndValue()
+
+	if len(res) > 10 {
+		res = res[:10]
+	}
+
+	return res
 }
 
 func correctWord(word string) string {
-	return strings.Trim(strings.ToLower(word), puncts)
+	return strings.Trim(strings.ToLower(word), punctuation)
 }
