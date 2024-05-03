c3605c97d5141da011fd02df4a26fc3886052379
Иван Марочкин <marochkin_i@rusklimat.ru>
2024-05-03 10:51:49 +0300

    save hw3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,28 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
+import (
+	"github.com/wasilibs/go-re2"
+	"strings"
+)
+
+const puncts = `,.!?;:-"'¡¿`
+
+var wordSurroundedPunctuation = re2.MustCompile(`^[[:punct:]]+[[:graph:]]+[[:punct:]]+$`)
+
+func Top10(input string) []string {
+	words := strings.Fields(input)
+
+	rating := make(map[string]int)
+	for _, word := range words {
+		if wordSurroundedPunctuation.MatchString(word) {
+			word = correctWord(word)
+		}
+		rating[word]++
+	}
+
 	return nil
+}
+
+func correctWord(word string) string {
+	return strings.Trim(strings.ToLower(word), puncts)
 }
