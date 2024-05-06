11dbd3e668fae8ab868d20b0af05d6e8c92fe84d
Иван Марочкин <marochkin_i@rusklimat.ru>
2024-05-06 10:47:47 +0300

    complete first part - list start cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,16 +1,21 @@
 package hw04lrucache
 
+import "encoding/json"
+
 type Key string
 
+type CacheElement struct {
+	Key   Key
+	Value any
+}
+
 type Cache interface {
-	Set(key Key, value interface{}) bool
-	Get(key Key) (interface{}, bool)
+	Set(key Key, value any) bool
+	Get(key Key) (any, bool)
 	Clear()
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -22,4 +27,46 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value any) bool {
+	v, _ := json.Marshal(CacheElement{key, value})
+
+	if item, ok := c.items[key]; ok {
+		item.Value = v
+		c.queue.MoveToFront(item)
+		return true
+	}
+
+	item := c.queue.PushFront(v)
+	c.items[key] = item
+
+	if c.queue.Len() > c.capacity {
+		deleteItem := c.queue.Back()
+
+		var el CacheElement
+		_ = json.Unmarshal(deleteItem.Value.([]byte), &el)
+
+		delete(c.items, el.Key)
+	}
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (any, bool) {
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+
+		var el CacheElement
+		_ = json.Unmarshal(item.Value.([]byte), &el)
+
+		return el.Value, true
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
