99284a742cb932e0f45852f67d7d849b66c8b6b0
Иван Марочкин <marochkin_i@rusklimat.ru>
2024-06-25 18:23:16 +0300

    drafts

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -10,5 +10,17 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Place your code here.
+	stageIn := in
+	for _, stage := range stages {
+		out := stage(stageIn)
+		stageIn = out
+	}
+
+	select {
+	case <-done:
+		close(in)
+
+	}
+
 	return nil
 }
