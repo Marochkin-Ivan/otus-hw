fa93c87927301fae0180d63d9f4740dd6a31768c
Иван Марочкин <marochkin_i@rusklimat.ru>
2024-06-26 23:57:01 +0300

    write Copy func

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -11,5 +14,46 @@ var (
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	// Place your code here.
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	info, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if offset > info.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	buf := make([]byte, info.Size()-offset)
+	_, err = sourceFile.ReadAt(buf, offset)
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	destFile, err := os.OpenFile(toPath, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	buffer := bytes.NewBuffer(buf)
+
+	if limit == 0 || limit > int64(buffer.Len()) {
+		_, err = io.Copy(destFile, buffer)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err = io.CopyN(destFile, buffer, limit)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
