0d84b3dd6c8c19392fb73e827026830d6f557a7a
Иван Марочкин <marochkin_i@rusklimat.ru>
2024-06-27 21:54:20 +0300

    add tests

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+	t.Run("copy full file without params",
+		func(t *testing.T) {
+			tmp, _ := os.CreateTemp("", "output")
+			defer os.Remove(tmp.Name())
+
+			mustBe, _ := os.ReadFile("testdata/out_offset0_limit0.txt")
+
+			err := Copy("testdata/input.txt", tmp.Name(), 0, 0)
+			actual, _ := os.ReadFile(tmp.Name())
+
+			require.Nil(t, err)
+			require.Equal(t, mustBe, actual)
+		})
+
+	t.Run("offset > file length",
+		func(t *testing.T) {
+			err := Copy("testdata/input.txt", "", 100_000_000, 0)
+
+			require.EqualError(t, err, ErrOffsetExceedsFileSize.Error())
+		})
 }
