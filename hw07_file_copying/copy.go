1f2bd6be28386d1c3438ba05ae46c6803dca590b
Иван Марочкин <marochkin_i@rusklimat.ru>
2024-07-01 21:46:57 +0300

    add validation

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -12,30 +12,25 @@ import (
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrUnprocessableOffset   = errors.New("offset less than 0")
+	ErrUnprocessableLimit    = errors.New("limit less than 0")
+	ErrSameFiles             = errors.New("source and dest files are the same")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 {
+		return ErrUnprocessableOffset
+	}
+
+	if limit < 0 {
+		return ErrUnprocessableLimit
+	}
+
 	sourceFile, err := os.Open(fromPath)
 	if err != nil {
 		return err
 	}
 	defer sourceFile.Close()
-
-	info, err := sourceFile.Stat()
-	if err != nil {
-		return err
-	}
-
-	if offset > info.Size() {
-		return ErrOffsetExceedsFileSize
-	}
-
-	buf := make([]byte, info.Size()-offset)
-	_, err = sourceFile.ReadAt(buf, offset)
-	if err != nil && !errors.Is(err, io.EOF) {
-		return err
-	}
 
 	destFile, err := os.OpenFile(toPath, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0o666)
 	if err != nil {
@@ -43,7 +38,23 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 	defer destFile.Close()
 
-	buffer := bytes.NewBuffer(buf)
+	sourceFileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	destFileInfo, err := destFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if os.SameFile(sourceFileInfo, destFileInfo) {
+		return ErrSameFiles
+	}
+
+	if offset > sourceFileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
 
 	if limit == 0 || limit > int64(buffer.Len()) {
 		bar := progressbar.DefaultBytes(
