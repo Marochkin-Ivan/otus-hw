02bb76a8c26fbb46e0e06f0fc78cff027a66c548
Иван Марочкин <marochkin_i@rusklimat.ru>
2024-07-01 21:46:06 +0300

    process errors in test

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -11,22 +11,29 @@ func TestCopy(t *testing.T) {
 	// Place your code here.
 	t.Run("copy full file without params",
 		func(t *testing.T) {
-			tmp, _ := os.CreateTemp("", "output")
+			tmp, err := os.CreateTemp("", "output")
+			require.NoError(t, err)
 			defer os.Remove(tmp.Name())
 
-			mustBe, _ := os.ReadFile("testdata/out_offset0_limit0.txt")
+			mustBe, err := os.ReadFile("testdata/out_offset0_limit0.txt")
+			require.NoError(t, err)
 
-			err := Copy("testdata/input.txt", tmp.Name(), 0, 0)
-			actual, _ := os.ReadFile(tmp.Name())
+			err = Copy("testdata/input.txt", tmp.Name(), 0, 0)
+			require.NoError(t, err)
 
-			require.Nil(t, err)
+			actual, err := os.ReadFile(tmp.Name())
+			require.NoError(t, err)
+
 			require.Equal(t, mustBe, actual)
 		})
 
 	t.Run("offset > file length",
 		func(t *testing.T) {
-			err := Copy("testdata/input.txt", "", 100_000_000, 0)
+			tmp, err := os.CreateTemp("", "output")
+			require.NoError(t, err)
+			defer os.Remove(tmp.Name())
 
+			err = Copy("testdata/input.txt", tmp.Name(), 100_000_000, 0)
 			require.EqualError(t, err, ErrOffsetExceedsFileSize.Error())
 		})
 }
