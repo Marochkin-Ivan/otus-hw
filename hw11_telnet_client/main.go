c907466dae4213a0f65642a9d884da38b9fac2fb
Иван Марочкин <marochkin_i@rusklimat.ru>
2024-09-16 12:39:00 +0300

    complete task

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,52 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
+	var timeout time.Duration
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "connection timeout. Default: 10s")
+	flag.Parse()
+
+	if len(flag.Args()) < 2 {
+		log.Println(errors.New("not enough arguments to start telnet"))
+		return
+	}
+
 	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	// P.S. Do not rush to throw context down, think if it is useful with blocking operation?
+	addr := net.JoinHostPort(flag.Arg(0), flag.Arg(1))
+	telnet := NewTelnetClient(addr, timeout, os.Stdin, os.Stdout)
+
+	err := telnet.Connect()
+	if err != nil {
+		log.Println(err.Error())
+		os.Exit(1)
+	}
+	defer telnet.Close()
+
+	_, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err = telnet.Receive(); err != nil {
+			fmt.Fprintf(os.Stderr, "...Receive error: %v\n", err)
+			stop() // Завершаем контекст при ошибке
+		}
+	}()
+
+	if err = telnet.Send(); err != nil {
+		fmt.Fprintf(os.Stderr, "...Send error: %v\n", err)
+		stop() // Завершаем контекст при ошибке
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -14,8 +19,82 @@ type TelnetClient interface {
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	// Place your code here.
+	ctx, cancel := context.WithCancel(context.Background())
+	return &Client{
+		addr:    address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+		ctx:     ctx,
+		cancel:  cancel,
+	}
+}
+
+type Client struct {
+	addr    string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func (c *Client) Connect() error {
+	_, cancel := context.WithTimeout(c.ctx, c.timeout)
+	defer cancel()
+
+	conn, err := net.DialTimeout("tcp", c.addr, c.timeout)
+	if err != nil {
+		return fmt.Errorf("connection error: %w", err)
+	}
+
+	c.conn = conn
+	fmt.Fprintf(os.Stderr, "...Connected to %s\n", c.addr)
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *Client) Close() error {
+	fmt.Fprintln(os.Stderr, "...Connection closed")
+	c.cancel()
+	return c.conn.Close()
+}
+
+func (c *Client) Send() error {
+	scanner := bufio.NewScanner(c.in)
+
+	for scanner.Scan() {
+		select {
+		case <-c.ctx.Done():
+			return c.ctx.Err()
+
+		default:
+			_, err := c.conn.Write([]byte(scanner.Text() + "\n"))
+			if err != nil {
+				return fmt.Errorf("send message error: %w", err)
+			}
+		}
+	}
+
+	return scanner.Err()
+}
+
+func (c *Client) Receive() error {
+	scanner := bufio.NewScanner(c.conn)
+
+	for scanner.Scan() {
+		select {
+		case <-c.ctx.Done():
+			return c.ctx.Err()
+
+		default:
+			_, err := fmt.Fprintln(c.out, scanner.Text())
+			if err != nil {
+				return fmt.Errorf("receive message error: %w", err)
+			}
+		}
+	}
+
+	return scanner.Err()
+}
